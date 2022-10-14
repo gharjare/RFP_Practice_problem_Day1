5ccf23b7d9b77050f226813b3d69703fb4f157d1
gharjare <gharjarechetan@gmail.com>
2022-10-14 23:54:30 +0530

    program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace VowelAndConsanant
+{
+     class Program
+    {
+        public static void Main(string[] args)
+        {
+            //CheckAlphabet.Display();
+            //Swaping.SwapingTheNumbers();
+            //FindLargestNumber.CheckLargestNumber();
+            //QuotientAndRemainder.CheckQuotientAndRemainder();
+            FindEvenAndOddNumbers.CheckEvenAndOdd();
+        }
+    }
+}
