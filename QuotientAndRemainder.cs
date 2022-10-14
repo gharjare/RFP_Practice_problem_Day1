c6e79faff526a4fd178a77de343e3fef0fa5b74d
gharjare <gharjarechetan@gmail.com>
2022-10-15 00:06:26 +0530

    QuotientandRemainder Problem

---
+++ QuotientAndRemainder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelAndConsanant
+{
+    public static class QuotientAndRemainder
+    {
+        public static void CheckQuotientAndRemainder()
+        {
+            int divident = 50, divisor = 8;
+
+            int quotient = divident / divisor;
+            int remainder = divident % divisor;
+            Console.WriteLine("Divident : {0} Divisor : {1}", divident, divisor);
+            Console.WriteLine("Quotient : + quotient");
+            Console.WriteLine("Remainder : " + remainder);
+        }
+    }
+}
